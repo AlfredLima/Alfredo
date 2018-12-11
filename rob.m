1ae822d235e88a37542f993da99ae0c59fec2fce
AlfredoLima <alfredolms@ic.ufal.br>
2018-12-11 12:12:52 -0300

    Create rob.m

---
+++ rob.m
@@ -0,0 +1,13 @@
+
+C(c) = [ cos(c) -sin(c) 0 ; 
+    sin(c) cos(c) 0 ;
+    0 0 1 ]
+
+B(b) = [   cos(b)  0 sin(b) ; 
+        0       1 0 ; 
+        -sin(b) 0 cos(b)]
+
+A(a) = [   1   0       0 ; 
+        0   cos(a)  -sin(a) ; 
+        0   sin(a)  cos(a) ]
+    
